6fdc764ac85e077c2c8e62c99d9b2248b7031f6f
Anas Azeem <anas.android@boltt.net>
2016-02-24 16:51:41 +0530

    Renamed the MainActivity to NutritionActivity and fragment to NutritionMainFragment, same with their corresponding xmls.

--- app/src/main/java/com/android/boltt/activity/NutritionActivity.java
+++ app/src/main/java/com/android/boltt/activity/NutritionActivity.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 
 import com.android.boltt.R;
 
-public class MainActivity extends BaseActivity {
+public class NutritionActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_nutrition);
     }
 
 }
--- app/src/main/java/com/android/boltt/fragment/NutritionMainFragment.java
+++ app/src/main/java/com/android/boltt/fragment/NutritionMainFragment.java
@@ -8,17 +8,14 @@ import android.view.ViewGroup;
 
 import com.android.boltt.R;
 
-/**
- * A placeholder fragment containing a simple view.
- */
-public class MainActivityFragment extends Fragment {
+public class NutritionMainFragment extends Fragment {
 
-    public MainActivityFragment() {
+    public NutritionMainFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.fragment_nutrition_main, container, false);
     }
 }
